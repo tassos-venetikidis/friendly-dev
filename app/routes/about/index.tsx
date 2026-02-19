276bf43aa14dc96e6b30e6642c0b70e5b0a44e2d
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-02-19 22:17:04 +0200

    Changed the fragment tags to section tags

--- app/routes/about/index.tsx
+++ app/routes/about/index.tsx
@@ -1,8 +1,8 @@
 function AboutPage() {
   return (
-    <>
+    <section>
       <h2 className="text-3xl font-bold mb-2">Hey, I'm Tassos 👋</h2>
-    </>
+    </section>
   );
 }
 
--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -8,5 +8,5 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <>My App</>;
+  return <section>My App</section>;
 }
