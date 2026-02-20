74f4033c2d94d1e0b39e393450f9d66c94f76cb5
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-02-20 17:10:30 +0200

    Changed the containing tag of all the route page components, except for the home page, from a section tag to a fragment

--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -1,8 +1,8 @@
 function ContactPage() {
   return (
-    <section>
+    <>
       <h2 className="text-3xl font-bold text-center mb-8">📫 Contact Me</h2>
-    </section>
+    </>
   );
 }
 
--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,8 +1,8 @@
 function ProjectsPage() {
   return (
-    <section>
+    <>
       <h2 className="text-3xl font-bold mb-8">🚀 Projects</h2>
-    </section>
+    </>
   );
 }
 
