f0377db0b9efb51c92d53eb71043aa1f23e48cab
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-02-22 16:52:17 +0200

    Implemented the project details route and the accompanying page display

---
+++ app/routes/projects/details.tsx
@@ -0,0 +1,62 @@
+import type { Route } from "./+types/details";
+import type { Project } from "~/types";
+import { Link } from "react-router";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
+export async function clientLoader({
+  request,
+  params,
+}: Route.ClientLoaderArgs): Promise<Project> {
+  const res = await fetch(`http://localhost:8000/projects/${params.id}`);
+  if (!res.ok) throw new Response("Project not found", { status: 404 });
+  const project: Project = await res.json();
+
+  return project;
+}
+
+export function HydrateFallback() {
+  return <div>Loading...</div>;
+}
+
+function ProjectDetailsPage({ loaderData }: Route.ComponentProps) {
+  const project = loaderData;
+  return (
+    <>
+      <Link
+        to="/projects"
+        className="flex items-center text-blue-400 hover:text-blue-500 transition mb-6"
+      >
+        <FaArrowLeft className="mr-2" /> Back To Projects
+      </Link>
+
+      <div className="grid gap-8 md:grid-cols-2 items-start">
+        <div>
+          <img
+            src={project.image}
+            alt={project.title}
+            className="w-full rounded-lg shadow-md"
+          />
+        </div>
+        <div>
+          <h1 className="text-3xl font-bold text-blue-400 mb-4">
+            {project.title}
+          </h1>
+          <p className="text-gray-300 text-sm mb-4">
+            {new Date(project.date).toLocaleDateString()} &middot;{" "}
+            {project.category}
+          </p>
+          <p className="text-gray-200 mb-6">{project.description}</p>
+          <a
+            href={project.url}
+            target="_blank"
+            className="inline-block text-white bg-blue-600 hover:bg-blue-700 px-6 py-2 rounded transition"
+          >
+            View Live Site <FaArrowRight className="inline" />
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ProjectDetailsPage;
