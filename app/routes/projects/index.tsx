99a62ef12a398ce472d42cbdad5ce43007e2ffcb
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-02-22 15:08:00 +0200

    Set up a loader in the projects index route to fetch projects from the json-server

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,4 +1,18 @@
-function ProjectsPage() {
+import type { Route } from "./+types/index";
+import type { Project } from "~/types";
+
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
+  const res = await fetch("http://localhost:8000/projects");
+  const data = await res.json();
+
+  return { projects: data };
+}
+
+function ProjectsPage({ loaderData }: Route.ComponentProps) {
+  const { projects } = loaderData as { projects: Project[] };
+
   return (
     <>
       <h2 className="text-3xl font-bold mb-8">🚀 Projects</h2>
