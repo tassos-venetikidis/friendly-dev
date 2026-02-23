a7d6b883a05a717d1f29fe950a5990517ca76009
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-02-23 15:32:30 +0200

    Added pagination logic in the projects index route and created a Pagination component that implements it

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import type { Route } from "./+types/index";
 import type { Project } from "~/types";
 import ProjectCard from "~/components/ProjectCard";
+import Pagination from "~/components/Pagination";
 
 export async function loader({
   request,
@@ -14,14 +16,28 @@ export async function loader({
 function ProjectsPage({ loaderData }: Route.ComponentProps) {
   const { projects } = loaderData as { projects: Project[] };
 
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // Pagination Calculations
+  const projectsPerPage = 4;
+  const totalPages = Math.ceil(projects.length / projectsPerPage);
+  const indexOfFirst = currentPage * projectsPerPage - projectsPerPage;
+  const indexOfLast = currentPage * projectsPerPage;
+  const currentProjects = projects.slice(indexOfFirst, indexOfLast);
+
   return (
     <>
       <h2 className="text-3xl font-bold mb-8">🚀 Projects</h2>
       <div className="grid gap-6 sm:grid-cols-2">
-        {projects.map((project) => (
+        {currentProjects.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
       </div>
+      <Pagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
     </>
   );
 }
