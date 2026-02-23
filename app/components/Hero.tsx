75addd4477b842dfcbf500bdea4175667569b40e
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-02-23 15:37:09 +0200

    Added and applied the HeroProps type for the Hero component

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,9 +1,14 @@
 import { Link } from "react-router";
 
+type HeroProps = {
+  name: string;
+  text: string;
+};
+
 function Hero({
   name = "[NAME]",
   text = "I build friendly web experiences and help others become confident modern developers.",
-}) {
+}: HeroProps) {
   return (
     <header className="text-center py-20 px-4 bg-gray-900 text-white transition-colors duration-300">
       <h2 className="text-4xl font-bold mb-4">Hey, I'm {name} 👋</h2>
