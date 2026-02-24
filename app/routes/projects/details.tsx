b6415b596b85a822cd442d9dd2fe636eee1c5d77
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-02-24 22:19:48 +0200

    Switched the base API URL to an environment variable and used it in the corresponding fetch requests

--- app/routes/projects/details.tsx
+++ app/routes/projects/details.tsx
@@ -7,7 +7,9 @@ export async function clientLoader({
   request,
   params,
 }: Route.ClientLoaderArgs): Promise<Project> {
-  const res = await fetch(`http://localhost:8000/projects/${params.id}`);
+  const res = await fetch(
+    `${import.meta.env.VITE_API_URL}/projects/${params.id}`,
+  );
   if (!res.ok) throw new Response("Project not found", { status: 404 });
   const project: Project = await res.json();
 
