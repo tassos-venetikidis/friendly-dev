c254ebacdb535869f2b768a7c70ed0684c3d0e7c
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-02-24 22:56:07 +0200

    Created a FeaturedProjects component and implemented its usage in the home page

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/index";
-import Hero from "~/components/Hero";
+import type { Project } from "~/types";
+import FeaturedProjects from "~/components/FeaturedProjects";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,6 +9,23 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
-  return <>Home Page</>;
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/projects`);
+  const data = await res.json();
+
+  return { projects: data };
 }
+
+function HomePage({ loaderData }: Route.ComponentProps) {
+  const { projects } = loaderData;
+
+  return (
+    <>
+      <FeaturedProjects projects={projects} count={2} />
+    </>
+  );
+}
+
+export default HomePage;
