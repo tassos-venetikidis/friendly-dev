b9673dbb90ea3f3b163192d8a3f438efa4815619
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-02-25 15:45:24 +0200

    Added the markup of the contact form in the contact page

--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -1,8 +1,71 @@
 function ContactPage() {
   return (
-    <>
+    <div className="max-w-3xl mx-auto mt-12 px-6 py-8 bg-gray-900">
       <h2 className="text-3xl font-bold text-center mb-8">📫 Contact Me</h2>
-    </>
+      <form className="space-y-6">
+        <div>
+          <label
+            htmlFor="name"
+            className="block text-sm font-medium text-gray-300"
+          >
+            Full Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-300"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="subject"
+            className="block text-sm font-medium text-gray-300"
+          >
+            Subject
+          </label>
+          <input
+            type="text"
+            name="subject"
+            id="subject"
+            className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="message"
+            className="block text-sm font-medium text-gray-300"
+          >
+            Message
+          </label>
+          <textarea
+            name="message"
+            id="message"
+            className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-blue-600 text-white py-2 rounded-lg cursor-pointer hover:bg-blue-700"
+        >
+          Send Message
+        </button>
+      </form>
+    </div>
   );
 }
 
