a04beaf211c06c4adb5db4107f21187f928af86c
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-02-25 15:25:05 +0200

    Created an AboutPreview component and added it in the home page

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,6 +1,7 @@
 import type { Route } from "./+types/index";
 import type { Project } from "~/types";
 import FeaturedProjects from "~/components/FeaturedProjects";
+import AboutPreview from "~/components/AboutPreview";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -24,6 +25,7 @@ function HomePage({ loaderData }: Route.ComponentProps) {
   return (
     <>
       <FeaturedProjects projects={projects} count={2} />
+      <AboutPreview />
     </>
   );
 }
