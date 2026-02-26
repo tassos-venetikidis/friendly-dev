0075c31dfe56819b55ce253a3c81c405a8c3c0da
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-02-26 15:38:49 +0200

    Implemented a loader function in the Blog page that fetchs the blog post metadata array

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,4 +1,19 @@
-function BlogPage() {
+import type { Route } from "./+types/index";
+import type { PostMeta } from "~/types";
+import { Link } from "react-router";
+
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
+  const url = new URL("/posts-meta.json", request.url);
+  const res = await fetch(url.href);
+  if (!res.ok) throw new Error("Failed to fetch data");
+  const data = await res.json();
+  return { posts: data };
+}
+
+function BlogPage({ loaderData }: Route.ComponentProps) {
+  const { posts } = loaderData;
   return (
     <>
       <h2 className="text-3xl font-bold mb-8">📝 Blog</h2>
