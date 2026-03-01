90c11bddc6833fc8cbb001b6635b513d190ae289
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-03-01 15:54:15 +0200

    Added pagination in the index.tsx of the blog page by reusing the Pagination component

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import Pagination from "~/components/Pagination";
 import type { Route } from "./+types/index";
 import type { PostMeta } from "~/types";
 import PostCard from "~/components/PostCard";
@@ -19,14 +21,30 @@ export async function loader({
 }
 
 function BlogPage({ loaderData }: Route.ComponentProps) {
+  const [currentPage, setCurrentPage] = useState(1);
   const { posts } = loaderData;
+
+  // Pagination Calculations
+  const postPerPage = 4;
+  const totalPages = Math.ceil(posts.length / postPerPage);
+  const indexOfFirst = currentPage * postPerPage - postPerPage;
+  const indexOfLast = currentPage * postPerPage;
+  const currentPosts = posts.slice(indexOfFirst, indexOfLast);
+
   return (
-    <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
-      <h2 className="text-3xl font-bold mb-8">📝 Blog</h2>
-      {posts.map((post) => (
-        <PostCard key={post.slug} post={post} />
-      ))}
-    </div>
+    <>
+      <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
+        <h2 className="text-3xl font-bold mb-8">📝 Blog</h2>
+        {currentPosts.map((post) => (
+          <PostCard key={post.slug} post={post} />
+        ))}
+      </div>
+      <Pagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+    </>
   );
 }
 
